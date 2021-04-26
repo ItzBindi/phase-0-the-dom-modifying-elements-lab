837d47090ec5e5cca8d39c8013a45dd4f9ebd6df
bindi <snair306@gmail.com>
2021-04-25 21:30:25 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+
+//Const body = document.getElementById(‘main’);
+//body.remove(main.querySelector(‘#main’);
+
+//const newHeader = document.createElement(‘h1’)
+//newHeader.setAttribute("id", "victory");
+//document.body.appendChild(newHeader);
+//newHeader.innerHTML = "Suraj is the champion"
+
+main.remove()
+
+const newHeader =
+document.createElement('h1')
+
+newHeader.id= 'victory';
+
+newHeader.innerHTML = "Suraj is the champion";
